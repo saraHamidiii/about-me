aea763081f963ceabcce5a95d51674ef7031cc9a
Sara Hamidi <Sara.Hamidi01@student.csulb.edu>
2025-05-13 20:32:02 -0700

    delete unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import Header from "./components/header";
 import ProjectTiles from "./components/projectTiles";
 import Nav from "./components/navBar";
